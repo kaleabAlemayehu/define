1072468f7b41015b242f8672277e7a626095b328
cipher <cipherlofi04@gmail.com>
2023-03-24 01:14:46 +0300

    Structuring the needed data by adding field.js

---
+++ fields.js
@@ -0,0 +1,17 @@
+export class Definition {
+  constructor(wordType, shortDefinition) {
+    this.wordType = wordType;
+    this.shortDefinition = shortDefinition;
+  }
+}
+
+export class Synonym {
+  constructor(synon) {
+    this.synon = synon;
+  }
+}
+export class Antonym {
+  constructor(anton) {
+    this.anton = anton;
+  }
+}
--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 import fs from "fs";
 import dotenv from "dotenv";
 import path from "path";
+// import { Antonym } from "./fields";
+import { Definition, Synonym, Antonym } from "./fields.js";
 
 dotenv.config();
 
@@ -13,8 +15,34 @@ const urlThe = `https://www.dictionaryapi.com/api/v3/references/thesaurus/json/$
   try {
     const responseDif = await axios.get(urlDic);
     const responseThe = await axios.get(urlThe);
-    Array.from(responseDif.data).forEach((element, index) => {});
-    Array.from(responseThe.data).forEach((element, index) => {});
+
+    responseDif.data.forEach((element, index) => {
+      // const shortDefinition = new Definition(element.fl, element.shortdef);
+      // console.log(`the short definition: ${JSON.stringify(shortDefinition)}`);
+      //   fs.writeFile(
+      //     `${path.resolve("./")}/define${index}.json`,
+      //     JSON.stringify(element),
+      //     (err) => {
+      //       if (err) {
+      //         console.log(err);
+      //       }
+      //     }
+      //   );
+    });
+    responseThe.data.forEach((element, index) => {
+      const synon = new Synonym(JSON.stringify(element.meta.syns));
+      // console.log(...element.meta.syns);
+      console.log(`The synon are: ${synon}`);
+      // fs.writeFile(
+      //   `${path.resolve("./")}/syns${index}.json`,
+      //   JSON.stringify(...element.meta.syns),
+      //   (err) => {
+      //     if (err) {
+      //       console.log(err);
+      //     }
+      //   }
+      // );
+    });
   } catch (e) {
     console.error(e);
   }
